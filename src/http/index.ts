f8df80281954c4dad56dee6ce4671543461f068a
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2024-09-23 15:26:41 +0300

    Refactor ESLint configuration and Vite plugins

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "sonner";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+const baseURL = import.meta.env.VITE_API_URL || "/api";
 
 const http = axios.create({ baseURL });
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import useSWR from "swr";
 
 const Index = () => {
-  const { data } = useSWR("/api");
+  const { data } = useSWR("/todos");
   console.log(data);
-  return <div>Index</div>;
+  return (
+    <div className="">
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 };
 
 export default Index;
+
+export const Server = async () => {
+  return <div>sadklas</div>;
+};
