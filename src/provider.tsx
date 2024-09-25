1f82ace9ee27460bf74fa96ca9830f098bfe4d62
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2024-09-25 14:25:32 +0300

    Refactor ESLint configuration, Vite plugins, and add TypeScript support

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,10 +1,12 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useNavigate } from "react-router-dom";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
   return (
-    <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="dark">
+    <NextUIProvider navigate={navigate} validationBehavior="native">
+      <NextThemesProvider attribute="class" defaultTheme="light">
         {children}
       </NextThemesProvider>
     </NextUIProvider>
